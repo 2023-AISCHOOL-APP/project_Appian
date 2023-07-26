85bd5ad6adc970843e23f40d931ace99747dfcea
julie-jihyelee <jh15693@naver.com>
2023-07-26 16:56:56 +0900

    React 프로젝트 생성

---
+++ React/project01/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      APP반, App이앙조 프로젝트입니다. 룰루!
+    </div>
+  );
+}
+
+export default App;

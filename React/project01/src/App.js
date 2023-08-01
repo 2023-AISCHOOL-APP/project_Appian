086927510bee34b704405f728ab5fda0e29d125c
julie-jihyelee <jh15693@naver.com>
2023-08-01 14:50:28 +0900

    테이블 명세 업데이트, 리액트 -캘린더 위젯, 폰트 추가

--- React/project01/src/App.js
+++ React/project01/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import './Components/CalendarDatePick'
+import CalendarDatePick from './Components/CalendarDatePick';
 
 function App() {
   return (
     <div className="App">
       APP반, App이앙조 프로젝트입니다. 룰루!
+    <CalendarDatePick/>
     </div>
   );
 }
---
+++ React/project01/src/Components/CalendarDatePick.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import DatePicker from 'react-datepicker';
+import '../Css/CalendarDatePick.css';
+import { ko } from "date-fns/esm/locale";
+
+const CalendarDatePick = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  // 게시물 작성 시 날짜를 선택하는 기능입니다. 
+  // input field 클릭 -> 하단에 캘린더 위젯 생성 -> 날짜 선택 -> 위젯 소멸 및 날짜 입력 
+  
+	return (
+		<div>
+			<DatePicker
+        locale={ko}
+			  selected={startDate}
+			  onChange={(date) => setStartDate(date)}
+			  dateFormat="yyyy-MM-dd"
+			/>
+		</div>
+  );
+};
+
+
+export default CalendarDatePick

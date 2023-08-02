89f10841d91b6b5d87f81b3f4a5747757011e53e
Mina Song <pksspink@naver.com>
2023-08-02 10:11:34 +0900

    로그인/회원가입추가
    로그인/회원가입 페이지 추가 , 폰트woff2 제거

--- React/project01/src/Main.js
+++ React/project01/src/Main.js
@@ -6,6 +6,8 @@ import Community from './pages/Community';
 import Mypage from './pages/Mypage';
 import OutGarden from './pages/OutGarden';
 import Mainpage from './Mainpage'
+import Join from './pages/Join';
+import Login from './pages/Login';
 
 function Main() {
   return (
@@ -14,16 +16,23 @@ function Main() {
         <Link to={'/'} className='logo'>
           <img src="img/logo.png" alt="Logo" />
         </Link>
-        <div className='navbar'>
 
+        <div className='navbar1'> 
+          <Link to={'/login'} className='navbarlogin'>로그인</Link>
+          <Link to={'/join'} className='navbarlogin'>회원가입</Link>
+        </div>
+
+        <div className='navbar'>
+         
           <Link to={'/find'} className='navbarMenu'>텃밭구하기</Link>
-          
           <Link to={'/out'} className='navbarMenu' >텃밭내놓기</Link>
           <Link to={'/community' } className='navbarMenu'>커뮤니티</Link>
           <Link to={'/mypage'} className='navbarMenu'>마이페이지 </Link>
         </div>
         <Routes>
           <Route path='/' element={<Mainpage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/join' element={<Join />} />
           <Route path='/find' element={<FindGarden />} />
           <Route path='/out' element={<OutGarden />} />
           <Route path='/community' element={<Community />} />
--- React/project01/src/index.js
+++ React/project01/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './Main';
+
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <Main />
-
+ <Main />
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function

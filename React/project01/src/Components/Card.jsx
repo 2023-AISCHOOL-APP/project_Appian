c44519941905be161e4285372fd3874a38e0fea9
julie-jihyelee <jh15693@naver.com>
2023-08-03 18:35:25 +0900

    검색 조건버튼, 지도기능, 상세 카드형 배치

---
+++ React/project01/src/Components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../Css/Card.css'
+import CardItem from './CardItem'
+
+const Card = ({uSido, uSigungu}) => {
+  return (
+    <div>
+      <h2>조건에 맞는 링크 좌라락</h2>
+    <div className='cardContainer'>
+
+      
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Card

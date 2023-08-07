bc65358d96a5989ed1442f7bed2f4f68d8549fce
kun-sik <58356215+kun-sik@users.noreply.github.com>
2023-08-07 11:22:37 +0900

    게시판 ing
    푸시

--- React/project01/src/Components/Cone.jsx
+++ React/project01/src/Components/Cone.jsx
@@ -1,48 +1,72 @@
 import React, { useState } from 'react';
 
-const Cone = () => {
+const WritingPage = ({ onAddCard }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [posts, setPosts] = useState([]);
+  const [imageURL, setImageURL] = useState('');
 
-  const handleAddPost = () => {
-    if (title.trim() !== '' && content.trim() !== '') {
-      const newPost = { id: Date.now(), title, content };
-      setPosts([...posts, newPost]);
+  const handleAddCard = () => {
+    if (title.trim() !== '' && content.trim() !== '' && imageURL.trim() !== '') {
+      onAddCard({ title, content, imageURL });
       setTitle('');
       setContent('');
+      setImageURL('');
     }
+  };
+
+  return (
+    <div className='writing-page'>
+      <h1>글 작성</h1>
+      <input
+        type="text"
+        placeholder="제목"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        rows="5"
+        placeholder="내용"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="사진 URL"
+        value={imageURL}
+        onChange={(e) => setImageURL(e.target.value)}
+      />
+      <button onClick={handleAddCard}>카드 추가</button>
+    </div>
+  );
+};
+
+const Cone = () => {
+  const [isWriting, setIsWriting] = useState(false);
+  const [cards, setCards] = useState([]);
+
+  const handleAddCard = (card) => {
+    setCards([...cards, card]);
+    setIsWriting(false);
   };
 
   return (
     <div>
       <h1 className='conetitle'>텃밭 자랑하기</h1>
-      <div className='ctextbox'>
-        제목 : <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <div>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^</div>
-        <textarea
-          rows="20"
-          cols="100"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        >
-          그대의 텃밭을 자랑해보시오
-        </textarea>
-        <div>
-        <button onClick={handleAddPost}>글 작성</button>
+      {!isWriting && <button className='write-button' onClick={() => setIsWriting(true)}>글 작성</button>}
+      {isWriting && <WritingPage onAddCard={handleAddCard} />}
+      <div className='card-list'>
+        <h2 className='ctexttitle'>게시판</h2>
+        <div className='card-container'>
+          {cards.map((card) => (
+            <div key={card.id} className='card'>
+              <img src={card.imageURL} alt={card.title} />
+              <h3>{card.title}</h3>
+              <p>{card.content.split('\n').map((line, index) => <span key={index}>{line}<br /></span>)}</p>
+            </div>
+          ))}
         </div>
       </div>
-      <div className='post-list'>
-        <h2 className='ctexttitle'>게시판</h2>
-        <ul className='ctext'>
-          {posts.map((post) => (
-            <li key={post.id}>
-              <strong className='ctexta'>{post.title}</strong>
-              <p className='ctexta'>{post.content}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+    
     </div>
   );
 };

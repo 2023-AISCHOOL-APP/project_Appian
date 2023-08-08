512d27cc8235ac1569ff41d2df223cf85b291655
julie-jihyelee <jh15693@naver.com>
2023-08-08 10:49:10 +0900

    메인페이지 스크롤 해결
    스크롤 이슈 해결, 브라우저라우터 인덱스로 옮기고 정리

--- React/project01/src/pages/FindGarden.jsx
+++ React/project01/src/pages/FindGarden.jsx
@@ -1,18 +1,27 @@
 // FindGarden.js
 import React from 'react';
 import GetAddress from '../Components/GetAddress';
+import '../Css/GetAddress.css';
 
 function FindGarden() {
   const style={
     backgroundColor : 'white'
   }
   return (
-    
+    <>
+    <div className='infopage'>
+      <div>
+        <span className='infotitle'>텃밭 검색하기</span>
+        <hr className='infohr'/>
+        <img src='/img/titlebg.png' className='infotitle_bg'/>
+      </div>
+    </div>
     <div style={style}>
       <div className='find'>
       <GetAddress/>
       </div>
     </div>
+    </>
   );
 }
 

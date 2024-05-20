dfcd9a5e2dc7e9f00388edda24e635507871e428
SpringDream0406 <springdream0406@gmail.com>
2024-05-21 05:34:40 +0900

    api..

--- React/project01/src/api_url.js
+++ React/project01/src/api_url.js
@@ -1,6 +1,6 @@
 // const API_URL = "http://localhost:3333";
 // const API_URL = "http://localhost:80";
-const API_URL = "http://13.209.93.158";
-// const API_URL = "farmfarmServer";
+// const API_URL = "http://13.209.93.158";
+const API_URL = "farmfarmServer";
 
 export default API_URL;
